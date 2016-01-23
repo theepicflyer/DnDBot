173f6850638d55d9755d23b3cabcc7a66e3a47fb
theepicflyer <xiejc2000@gmail.com>
2016-01-24 02:30:19 +0800

    Cleanup and documentation

--- monsters.py
+++ monsters.py
@@ -1,45 +1,43 @@
 from telegram import *
 
-updater = Updater(token="129193826:AAHfEbo7HjUhk5T48PC6gEhPgsIEYP8iuNI")
+updater = Updater(token="TOKEN")
 dispatcher = updater.dispatcher
 
-monsterList = []
-monsterIndex = 0
+monsterList = [] #list storing all monsters
+monsterIndex = 0 #number of monsters
 
 class Monster(object):
-    strength = 5
     def __init__(self, name, health):
         self.name = name
         self.health = health
-        
-    
-def monsterStats(bot, update):
-    findMonsterIndex(update.message.text[14:])
-    
-    print (monster)
-    print (monster.health)
 
-def findMonsterIndex(name):
+def findMonsterIndex(name): #returns index of character using player name
     for i in range(len(monsterList)):
         if name == monsterList[i].name:
             return i
 
 def createMonster(bot, update):
     global monsterIndex
-    monsterName = update.message.text[15:]
-    health = 5
-    monsterList.append(Monster(monsterName, health))
-    print (monsterList[monsterIndex].name)
+    input = update.message.text #input: /createmonster NAME HEALTH
+    input = input.split() #split the input into the 3 parts
+    monsterName = input[1]
+    health = int(input[2])
+    monsterList.append(Monster(monsterName, health)) #add to list
     bot.sendMessage(chat_id = update.message.chat_id, text = monsterList[monsterIndex].name + " has been created")
     monsterIndex += 1
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Hello")
     
-#def attackMonster(bot, update):
-
+def attackMonster(bot, update):
+    input = update.message.text #input: /attackmonster NAME DAMAGE
+    input = input.split() #split into the parts
+    i = findMonsterIndex(input[1])
+    damage = int(input[2])
+    monsterList[i].health -= damage
+    bot.sendMessage(chat_id = update.message.chat_id, text = monsterList[i].name + "'s health has reduced by " + str(damage) + " to " + str(monsterList[i].health))
+    
 dispatcher.addTelegramCommandHandler('createmonster', createMonster)
-dispatcher.addTelegramCommandHandler('monsterstats', monsterStats)
 dispatcher.addTelegramCommandHandler('start', start)
-#dispatcher.addTelegramCommandHandler('attackmonster', attackMonster)
+dispatcher.addTelegramCommandHandler('attackmonster', attackMonster)
 updater.start_polling()
