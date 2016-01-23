4072c8759865bd1017e7fae67c97ac2f9797f162
DM-Lee <ryanlee7240@gmail.com>
2016-01-24 02:32:51 +0800

    Update CreateCharacterFinal.py

--- CreateCharacterFinal.py
+++ CreateCharacterFinal.py
@@ -1,5 +1,3 @@
-#remember checking for race and class
-
 from telegram import *
 
 updater = Updater(token="INSERT YOUR TOKEN HERE")
@@ -22,7 +20,7 @@ class Character(object):
         self.characterName = characterName
 
     def updateStats(self, race, _class):
-        print ("works!")
+        #Race Stats for Human
         if self.race == 'human':
             self.stats['strength'] = 5
             self.stats['dexterity'] = 5
@@ -30,6 +28,7 @@ class Character(object):
             self.stats['intelligence'] = 5
             self.stats['constitution'] = 5
             self.stats['charisma'] = 5
+        #Race Stats for Dwarf
         elif self.race == 'dwarf':
             self.stats['strength'] = 6
             self.stats['dexterity'] = 3
@@ -37,6 +36,7 @@ class Character(object):
             self.stats['intelligence'] = 3
             self.stats['constitution'] = 7
             self.stats['charisma'] = 3
+        #Race Stats for Elf
         elif self.race == 'elf':
             self.stats['strength'] = 3
             self.stats['dexterity'] = 3
@@ -44,6 +44,7 @@ class Character(object):
             self.stats['intelligence'] = 7
             self.stats['constitution'] = 3
             self.stats['charisma'] = 6
+        #Class Stats for Fighter
         if self._class == 'fighter':
             self.stats['strength'] = self.stats['strength'] + 4
             self.stats['dexterity'] = self.stats['dexterity'] 
@@ -51,6 +52,7 @@ class Character(object):
             self.stats['intelligence'] = self.stats['intelligence'] - 2
             self.stats['constitution'] = self.stats['constitution']
             self.stats['charisma'] = self.stats['charisma'] - 1
+        #Class Stats for Mage
         elif self._class == 'mage':
             self.stats['strength'] = self.stats['strength'] - 2
             self.stats['dexterity'] = self.stats['dexterity'] + 1
@@ -58,6 +60,7 @@ class Character(object):
             self.stats['intelligence'] = self.stats['intelligence'] + 2
             self.stats['constitution'] = self.stats['constitution'] - 2
             self.stats['charisma'] = self.stats['charisma']
+        #Class Stats for Thief
         elif self._class == 'thief':
             self.stats['strength'] = self.stats['strength'] - 1
             self.stats['dexterity'] = self.stats['dexterity'] + 2
@@ -68,6 +71,7 @@ class Character(object):
         print(str(self.stats))      
 
 def start(bot, update):
+    #Displays "Welcome to Dungeons and Dragons.")
     bot.sendMessage(chat_id = update.message.chat_id, text = "Welcome to Dungeons and Dragons.")
 
 def createCharacter(bot, update):
@@ -75,9 +79,11 @@ def createCharacter(bot, update):
     characterName = update.message.text[17:]
     playerName = update.message.from_user.first_name
     characterList.append(Character(playerName, characterName))
+    #Displays "Character [Character] has been created [Player]"
     bot.sendMessage(chat_id = update.message.chat_id, text = "Character " + characterList[playerIndex].characterName + " has been created by " + characterList[playerIndex].playerName)
     playerIndex += 1
-    bot.sendMessage(chat_id = update.message.chat_id, text = "@" + playerName + " Please enter your character's attributes in the format: race class")
+    #Displays "@[Player] Please enter your character's attributes in the format of [Race] [Class]"
+    bot.sendMessage(chat_id = update.message.chat_id, text = "@" + playerName + " Please enter your character's Race & Class in the format: [Race] [Class]")
     global attributes
     attributes = True
     print(str(attributes))
@@ -85,25 +91,23 @@ def createCharacter(bot, update):
 def incomingMessages(bot, update):
     global attributes
     if attributes == True:
-        print ("lols")
         attributesInput = update.message.text
         attributesInput = attributesInput.split()
-        print (attributesInput[0])
         i = findCharacterIndex(update.message.from_user.first_name)
         characterList[i].race = attributesInput[0]
         characterList[i]._class = attributesInput[1]
-        bot.sendMessage(chat_id = update.message.chat_id, text = "@" + characterList[i].playerName + " " + characterList[i].characterName + "'s race and class is " + characterList[i].race + " and " + characterList[i]._class)
+        #Display "@[Player] [Character]'s race is [Race] and [Character]'s class is [Class]
+        bot.sendMessage(chat_id = update.message.chat_id, text = "@" + characterList[i].playerName + " " + characterList[i].characterName + "'s race is " + characterList[i].race + " and " + characterList[i].characterName + "'s class is"+ characterList[i]._class) + "."
         characterList[i].updateStats(characterList[i].race, characterList[i]._class)
         printCharacterStats(bot, update)
         attributes = False
 
 def printCharacterStats(bot, update):
+    print("hello")
     i = findCharacterIndex(update.message.from_user.first_name)
-    text = str(characterList[i].characterName) + "\n Created by: " + str(characterList[i].playerName) + "\n Strength: " + str(characterList[i].stats['strength']) + "\n Dexterity: " + str(characterList[i].stats['dexterity']) + "\n Wisdom: " + str(characterList[i].stats['wisdom']) + "\n Intelligence: " + str(characterList[i].stats['intelligence']) + "\n Constitution: " + str(characterList[i].stats['constitution']) + "\n Charisma: " + str(characterList[i].stats['charisma'])
+    statsheet = str(characterList[i].characterName) + "\n Created by: " + str(characterList[i].playerName) + "\n Strength: " + str(characterList[i].stats['strength']) + "\n Dexterity: " + str(characterList[i].stats['dexterity']) + "\n Wisdom: " + str(characterList[i].stats['wisdom']) + "\n Intelligence: " + str(characterList[i].stats['intelligence']) + "\n Constitution: " + str(characterList[i].stats['constitution']) + "\n Charisma: " + str(characterList[i].stats['charisma'])
     bot.sendMessage(chat_id = update.message.chat_id, text = text) 
 
-
-# i = findCharacterIndex(update.message.from_user.first_name)   
 def findCharacterIndex(first_name):
     for i in range(len(characterList)):
         if characterList[i].playerName == first_name:
@@ -116,6 +120,6 @@ dispatcher.addTelegramMessageHandler(IncomingMessages)
 dispatcher.addTelegramCommandHandler('start', start)
 dispatcher.addTelegramCommandHandler('createcharacter', createCharacter)
 dispatcher.addTelegramCommandHandler('printcharacterstats', printCharacterStats)
-dispatcher.addUnknownTelegramCommandHandler(unknown)                
+dispatcher.addUnknownTelegramCommandHandler(unknown)
 
 updater.start_polling()
