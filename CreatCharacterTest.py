0272fc9bcfbbe9b6ebd2634c0014ef1d52074091
kaitlynng <kaitlyn.nky@gmail.com>
2016-01-23 18:16:36 +0800

    create character v2.0

--- CreatCharacterTest.py
+++ CreatCharacterTest.py
@@ -2,22 +2,34 @@ from telegram import Updater
 
 updater = Updater(token="152501487:AAElGigfjuICcLgXT4U2qu74OQyxmjQQ8Ho")
 dispatcher = updater.dispatcher
-characterlist = []
 
+characterlist = []
 
 class Character(object):
     playerCount = 0
-    def __init__(self, name):
-        self.name = name
+    playername = None
+    charactername = None
+    race = None
+    _class = None
+    stats = {}
+    def __init__(self, charactername, playername):
         playerCount += 1
+        self.playername = playername
+        self.charactername = charactername
 
 def start(bot, update):
-    bot.sendMessage(chat_id = update.message.chat_id, text = "This is kaitlynbot.")
+    bot.sendMessage(chat_id = update.message.chat_id, text = "Welcome to Dungeons and Dragons.")
 
 def createCharacter(bot, update):
-    update.message.from_user.first_name = Character(update.message.text)
-    characterlist[Character.playerCount] = Character()
-    bot.sendMessage(chat_id = update.message.chat_id, text = "The character %s has been created!" % (____.name))
+    charactername = update.message.text
+    playername = update.message.from_user.first_name
+    characterlist[Character.playerCount] = Character(charactername, playername)   
+    bot.sendMessage(chat_id = update.message.chat_id, text = "The character %s has been created!" % (charactername))
+
+def findCharacterIndex(bot, update):
+    for i in characterlist:
+        if characterlist[i].playername == update.message.from_user.first_name:
+            return characterlist[i]
 
 def unknown(bot, update):
     bot.sendMessage(chat_id = update.message.chat_id, text = "Sorry, I didn't understand that!")
@@ -29,4 +41,3 @@ dispatcher.addTelegramCommandHandler('createcharacter', createCharacter)
 dispatcher.addUnknownTelegramCommandHandler(unknown)                
 
 updater.start_polling()
-    
