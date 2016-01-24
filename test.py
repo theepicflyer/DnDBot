e79342ff35ebea118e5dc13cf6d20a8c95ac4384
theepicflyer <xiejc2000@gmail.com>
2016-01-24 13:36:30 +0800

    parse

--- test.py
+++ test.py
@@ -3,21 +3,13 @@ sdasd= "A B C D E"
 def parseInput(words, no):
     words = words.split()
     a = len(words) - no
-    print(words)
-    print(str(a))
-
     oup = words[1]
     for i in range(2, 2 + a):
         #oup += " "
         oup += words[i]
     outt = []
     outt.append(words[0])
-    outt[0] = words[0]
-    outt[1] = oup
-    for i in range(0, no):
-        outt[i] = words[a+2]
-    print(oup)
-
-parseInput(sdasd, 2)
-
-parseInput(sdasd, 5)
+    outt.append(oup)
+    for i in range(2, no):
+        outt.append(words[a + i])
+    return outt
